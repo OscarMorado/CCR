60d63495b0ab4d7b1d4b1175a99b0719e17a08c9
OscarMorado <bp998@outlook.com>
2021-11-30 00:22:00 -0600

    CCR Beta V3
    We are missing the credits
    We have an unfunctional level 2
    We need an inside-game menu
    We need ANIMATIONS!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,12 +17,18 @@ public class PlayerController : MonoBehaviour
 
 
     Vector3 startPosition1 = new Vector3(347.13f, .03591731f, -880.5236f);
-    Vector3 startPosition2 = new Vector3(349.1303f, .3118293f, -730.79f);
+    Vector3 startPosition2 = new Vector3(349.1303f, .51591731f, -730.79f);
     //limits:
-    private float limitZMax = -700.0f;
-    private float limitZMin = -880.0f;
-    private float limitXMax = 414.0f;
-    private float limitXMin = 286.0f;
+    private float limitZMax = -702.0f;
+    private float limitZMin = -879.0f;
+    private float limitXMax = 399.0f;
+    private float limitXMin = 300.0f;
+
+    private float limitZMax2 = -702.0f;
+    private float limitZMin2 = -879.0f;
+    private float limitXMax2 = 399.0f;
+    private float limitXMin2 = 300.0f;
+
     public bool isOnRiver;
     public bool nextStage = false;//Booleano para determinar si se alcanzo la meta
     public ParticleSystem food;
@@ -72,6 +78,7 @@ public class PlayerController : MonoBehaviour
    
     void Update()
     {
+        Scene scene = SceneManager.GetActiveScene();
         w=Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
         s=Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow);
         a=Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
@@ -117,7 +124,50 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.right);
             audioeffects.PlayOneShot(moving, 1.0f);
         
+        }else if(w && GameManagerScript.gameActive && transform.position.z>limitZMax2 && scene.name == "Level2"){   
+            playerAnim.Play("jump");
+            transform.Translate(Vector3.forward);//avanzar 1 unidad en z            
+            /*if (transform.position.z - lastPositionZ >= 1)
+            {//cada que se presiona la w debe aumentar su posicion en z para sumar
+                scoreManagerScript.score += 1;
+                lastPositionZ = transform.position.z;
+            }*/
+            //LastPositionZ guarda la posición más lejana a la cual llegó el jugador
+            //Con lastPositionLvl1 se verifica en qué nivel está Llv1 para el 1 y Lvl 2 para el 2
+            if (transform.position.z > lastPositionZ && lastPositionLvl1 == -880.5236f)
+            {//cada que se presiona la w score aumenta
+                scoreManagerScript.score += 1;
+                lastPositionZ = transform.position.z;
+            }
+            else if(transform.position.z < lastPositionZ && lastPositionLvl2 == -730.79f)
+            {//cada que se presiona la w debe aumentar su posicion en z para sumar
+                scoreManagerScript.score += 1;
+                lastPositionZ = transform.position.z;
+            }
+            //scoreManagerScript.score += 1;
+            audioeffects.PlayOneShot(moving, 1.0f);
+        }else if(s && GameManagerScript.gameActive && transform.position.z<limitZMin2 && scene.name == "Level2"){
+            playerAnim.Play("backwards");
+            //scoreManagerScript.score-=2; //Se le resta puntaje para que no se puedan farmear puntos
+            transform.Translate(Vector3.back);
+            scoreManagerScript.score -= 2;
+            audioeffects.PlayOneShot(moving, 1.0f);
+            
+        }else if(a && GameManagerScript.gameActive && transform.position.x<limitXMin2 && scene.name == "Level2" ){
+            playerAnim.Play("left");
+            transform.Translate(Vector3.left);
+            audioeffects.PlayOneShot(moving, 1.0f);
+            
+        }else if(d && GameManagerScript.gameActive && transform.position.x>limitXMax2 && scene.name == "Level2"){
+            playerAnim.Play("right");
+            transform.Translate(Vector3.right);
+            audioeffects.PlayOneShot(moving, 1.0f);
+        
         }
+
+        float x = transform.position.x;
+        float z = transform.position.z;
+        Debug.Log("X: " + x + "Z: "+ z);
     }
 
     public void resetPosition(){
@@ -155,21 +205,10 @@ public class PlayerController : MonoBehaviour
             GameManagerScript.FinalGoal();
         }
 
-        if (collision.gameObject.CompareTag("Construction")){  //Si entra a la zona de construcción, perderá una vida y tiempo
+        if (collision.gameObject.CompareTag("Construction") || collision.gameObject.CompareTag("GrassTrap")){  //Si entra a la zona de construcción, perderá una vida y tiempo
             resetPosition();
             scoreManagerScript.heartCounter -= 1;
             ScoreManager.time -=5;
-        }
-        IEnumerator trap(){
-            yield return new WaitForSecondsRealtime(1.0f);
-            if(collision.gameObject.CompareTag("GrassTrap")){
-                ScoreManager.time -=2;
-            }
-        }
-        StartCoroutine(trap());
-        if (!collision.gameObject.CompareTag("GrassTrap"))
-        {
-            StopCoroutine(trap());
         }
 
         if (collision.gameObject.CompareTag("TPSpawnLvl2")){
